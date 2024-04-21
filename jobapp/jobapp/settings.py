92f3cd275897f3bee1770e9233217a468c898e52
Oluwajimi06 <132276583+Oluwajimi06@users.noreply.github.com>
2024-04-21 02:38:50 +0100

    Update settings.py

--- jobapp/jobapp/settings.py
+++ jobapp/jobapp/settings.py
@@ -82,12 +82,16 @@ WSGI_APPLICATION = 'jobapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER':  'postgres',
+        'PASSWORD': 'YYNWTOEuRcvbMLRiRoWwzySZdXPhCDal',
+        'HOST': 'viaduct.proxy.rlwy.net',
+        'PORT': '35822',
+    }
+}
 
 
 # Password validation
