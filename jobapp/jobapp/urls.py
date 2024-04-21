2161659724d775abc24c06a556f7bf241bcbfa1b
Oluwajimi06 <132276583+Oluwajimi06@users.noreply.github.com>
2024-04-21 03:09:54 +0100

    Update urls.py

--- jobapp/jobapp/urls.py
+++ jobapp/jobapp/urls.py
@@ -25,6 +25,4 @@ urlpatterns = [
     path("jobs/", include('jobs.urls')),
     path("accounts/", include('accounts.urls')),
     path("accounts/", include("django.contrib.auth.urls")),
-]
-urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
